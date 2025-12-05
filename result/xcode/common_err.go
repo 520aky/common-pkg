6fbbba0b6da7221f2b8f52fd2d1fc3236b401709
520aky <520akyi@gmail.com>
2025-12-05 15:05:49 +0800

    err code

--- result/xcode/common_err.go
+++ result/xcode/common_err.go
@@ -4,6 +4,12 @@ import (
 	"net/http"
 )
 
+const (
+	SERVER_COMMON_ERROR = 100001
+	REQUEST_PARAM_ERROR = 100002
+	DB_ERROR            = 100003
+)
+
 var (
 	UserNotFound = New(http.StatusUnauthorized, "User not login. ")
 	TokenInvalid = New(http.StatusPaymentRequired, "Token invalid. ")
